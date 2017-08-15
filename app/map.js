4c8f5f4f2ad1f5bb4357953165d3726e418674d0
saashimi <saavedra.kevin@gmail.com>
2017-08-14 19:13:11 -0700

    fix wrong downloaded map.js file.

--- app/map.js
+++ app/map.js
@@ -11,54 +11,56 @@ var map = new mapboxgl.Map({
     center: [-122.682357, 45.508532], // starting position [lng, lat]
     zoom: 14, // starting zoom
 })
+var toggleableLayerIds = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 map.on('load', function() {
   map.addSource("sched_points", {
-    type: "geojson",
-    cluster: true,
-    data: "https://cdn.glitch.com/f385be6b-3d08-4de8-899d-247058842c55%2Ffull_sched.geojson?1502663523957",
-    clusterMaxZoom: 15, // Max zoom to cluster points on
-    clusterRadius: 20 // Use small cluster radius for the heatmap look
+    "type": "geojson",
+    "data": "https://cdn.glitch.com/f385be6b-3d08-4de8-899d-247058842c55%2Ffull_sched.geojson?1502663523957",
   });
   
   var dayLayer = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   var colLayer = ["red", "green", "blue", "orange", "gray", "purple"];
   
-   var layers = [
-        [5, 'green'],
-        [20, 'orange'],
-        [200, 'red']
-    ];
-
-    layers.forEach(function (layer, i) {
-        map.addLayer({
-            "id": "cluster-" + i,
-            "type": "circle",
-            "source": "sched_points",
-            "paint": {
-                "circle-color": layer[1],
-                "circle-radius": 70,
-                "circle-blur": 1 // blur the circles to get a heatmap look
-            },
-            "filter": i === layers.length - 1 ?
-                [">=", "point_count", layer[0]] :
-                ["all",
-                    [">=", "point_count", layer[0]],
-                    ["<", "point_count", layers[i + 1][0]]]
-        }, 'waterway-label' );
-    });
-
+  for (var i=0; i<dayLayer.length; i++) {
     map.addLayer({
-        "id": "unclustered-points",
-        "type": "circle",
-        "source": "sched_points",
-        "paint": {
-            "circle-color": 'rgba(0,255,0,0.5)',
-            "circle-radius": 20,
-            "circle-blur": 1
-        },
-        "filter": ["!=", "cluster", true]
-    }, 'waterway-label');
-      
+      "id" : dayLayer[i],
+      "type" : "circle",
+      "source": "sched_points",
+      'paint': {
+        'circle-radius': 4,
+        'circle-color': colLayer[i]  
+      }
+    });
+    map.setFilter(dayLayer[i], ["!=", "Day_start_"+dayLayer[i][0], "na"]);
+  }    
 })
 
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
+  
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
