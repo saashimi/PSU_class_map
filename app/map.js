df75dbcd80e7f9867b35bbb7737999e8c242671c
saashimi <saavedra.kevin@gmail.com>
2017-08-17 19:43:05 -0700

    separate heatmap layer display.

--- app/map.js
+++ app/map.js
@@ -12,31 +12,61 @@ var map = new mapboxgl.Map({
     zoom: 14, // starting zoom
 })
 var toggleableLayerIds = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var days = {"Mon": 'mon',
+            "Tue": 'tue',
+            "Wed": 'wed',
+            "Thu": 'thu',
+            "Fri": 'fri',
+            "Sat": 'sat'}
 
 map.on('load', function() {
-  map.addSource("sched_points", {
-    "type": "geojson",
-    "data": "https://cdn.glitch.com/f385be6b-3d08-4de8-899d-247058842c55%2Ffull_sched.geojson?1502663523957",
-    "cluster": true
-  });
-  
-  var dayLayer = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  var colLayer = ["red", "green", "blue", "orange", "gray", "purple"];
-  
-  for (var i=0; i<dayLayer.length; i++) {
-    map.addLayer({
-      "id" : dayLayer[i],
-      "type" : "circle",
-      "source": "sched_points",
-      'paint': {
-        'circle-radius': 4,
-        'circle-color': colLayer[i]  
-      }
+  for (var i=0; i<toggleableLayerIds.length; i++) {
+    console.log("data/" + days[toggleableLayerIds[i]]  + ".geojson");
+    map.addSource(days[toggleableLayerIds[i]], {
+      "type": "geojson",
+      "data": "data/" + days[toggleableLayerIds[i]]  + ".geojson",
+      "cluster": true
     });
-    map.setFilter(dayLayer[i], ["!=", "Day_start_"+dayLayer[i][0], "na"]);
-  }    
-})
+    var collayer = [
+          [5, 'green'],
+          [20, 'orange'],
+          [200, 'red']
+      ];
 
+  collayer.forEach(function (layer, j) {
+      map.addLayer({
+          "id": toggleableLayerIds[i] + j,
+          "type": "circle",
+          "source": days[toggleableLayerIds[i]],
+          "paint": {
+              "circle-color": layer[1],
+              "circle-radius": 70,
+              "circle-blur": 1 // blur the circles to get a heatmap look
+          },
+          "filter": j === collayer.length - 1 ?
+              [">=", "point_count", layer[0]]:
+              ["all",
+                  [">=", "point_count", layer[0]],
+                  ["<", "point_count", collayer[j + 1][0]]]
+      }, 'waterway-label' );
+
+  });
+
+  map.addLayer({
+      "id": toggleableLayerIds[i],
+      "type": "circle",
+      "source": days[toggleableLayerIds[i]],
+      "paint": {
+          "circle-color": 'rgba(0,255,0,0.5)',
+          "circle-radius": 20,
+          "circle-blur": 1
+      },
+      "filter": ["!=", "cluster", true]
+  }, 'waterway-label');
+
+  }
+  
+})
 
 for (var i = 0; i < toggleableLayerIds.length; i++) {
     var id = toggleableLayerIds[i];
@@ -59,8 +89,6 @@ for (var i = 0; i < toggleableLayerIds.length; i++) {
         } else {
             this.className = 'active';
             map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-            build_heatMap(clickedLayer);
-            console.log(clickedLayer);
         }
     };
   
@@ -68,41 +96,8 @@ for (var i = 0; i < toggleableLayerIds.length; i++) {
     layers.appendChild(link);
 }
 
+
 function build_heatMap(clickLayer) {
-  var collayer = [
-          [5, 'green'],
-          [20, 'orange'],
-          [200, 'red']
-      ];
+  
 
-  collayer.forEach(function (layer, i) {
-      map.addLayer({
-          "id": clickLayer + i,
-          "type": "circle",
-          "source": "sched_points",
-          "paint": {
-              "circle-color": layer[1],
-              "circle-radius": 70,
-              "circle-blur": 1 // blur the circles to get a heatmap look
-          },
-          "filter": i === collayer.length - 1 ?
-              [">=", "point_count", layer[0]]:
-              ["all",
-                  [">=", "point_count", layer[0]],
-                  ["<", "point_count", collayer[i + 1][0]]]
-      }, 'waterway-label' );
-  });
-
-  map.addLayer({
-      "id": "unclustered-points",
-      "type": "circle",
-      "source": "sched_points",
-      "paint": {
-          "circle-color": 'rgba(0,255,0,0.5)',
-          "circle-radius": 20,
-          "circle-blur": 1
-      },
-      "filter": ["!=", "cluster", true]
-  }, 'waterway-label');
-
-}
+} 
