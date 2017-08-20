950223347c47241c4ba96a5e368f2dbe635861a9
saashimi <saavedra.kevin@gmail.com>
2017-08-20 13:25:42 -0700

    latest progress with radio/slider.

--- app/map.js
+++ app/map.js
@@ -26,132 +26,56 @@ map.on('load', function() {
     "data": "data/PSU_buildings.geojson",
   });
 
-  for (var i=0; i<toggleableLayerIds.length; i++) {
+  map.addLayer({
+    "id": "buildings",
+    "source": "PSU_buildings",
+    "type": "fill",
+    "paint": {
+      "fill-color": "gray",
+      "fill-opacity": .5
+    }
+  })
+
+  for (var i=0; i<1; i++) {
+  //for (var i=0; i<toggleableLayerIds.length; i++) {
+
     map.addSource(days[toggleableLayerIds[i]], {
       "type": "geojson",
       "data": "data/" + days[toggleableLayerIds[i]]  + ".geojson",
-      "cluster": true
+      //"cluster": true
     });
+    //temp for visualization
     map.addLayer({
-      "id": "buildings",
-      "source": "PSU_buildings",
-      "type": "fill",
+      "id": toggleableLayerIds[i],
+      "type": "circle",
+      "source": days[toggleableLayerIds[i]],
       "paint": {
-        "fill-color": "gray",
-        "fill-opacity": .1
-      }
-    })
-
-    var collayer = [
-          [5, 'green'],
-          [25, 'orange'],
-          [200, 'red']
-      ];
-
-    var groupLayer = [];
-
-    collayer.forEach(function (layer, j) {
-        map.addLayer({
-            "id": toggleableLayerIds[i] + j,
-            "type": "circle",
-            "source": days[toggleableLayerIds[i]],
-            "paint": {
-                "circle-color": layer[1],
-                "circle-radius": 70,
-                "circle-blur": 1 // blur the circles to get a heatmap look
-            },
-            "filter": j === collayer.length - 1 ?
-                [">=", "point_count", layer[0]]:
-                ["all",
-                    [">=", "point_count", layer[0]],
-                    ["<", "point_count", collayer[j + 1][0]]]
-        }, 'waterway-label' );
-        groupLayer.push(toggleableLayerIds[i] + j)
-
+        "circle-color": "red",
+      },
+      "filter": ["has", "Day_start"]
     });
 
-    map.addLayer({
-        "id": toggleableLayerIds[i] + "-pt",
-        "type": "circle",
-        "source": days[toggleableLayerIds[i]],
-        "paint": {
-            "circle-color": 'rgba(0,255,0,0.5)',
-            "circle-radius": 20,
-            "circle-blur": 1
-        },
-        "filter": ["!=", "cluster", true]
-    }, 'waterway-label');
-  
-  groupLayer.push(toggleableLayerIds[i] + "-pt")
-  toggleLayer(groupLayer, toggleableLayerIds[i])
+    //end temp viz layer
+    document.getElementById('slider').addEventListener('input', function(e) {
+    // get the current hour as an integer
+    var hour = parseInt(e.target.value);
+    // map.setFilter(layer-name, filter)
 
+    map.setFilter('Mon', ['==', 'Day_start', hour]);
+
+    // converting 0-23 hour to AMPM format
+    var ampm = hour >= 12 ? 'PM' : 'AM';
+    var hour12 = hour % 12 ? hour % 12 : 12;
+    // update text in the UI
+    document.getElementById('active-hour').innerText = hour12 + ampm;
+    });
+  
   }
 
-})
 
-function toggleLayer(ids, name) {
-      var link = document.createElement('a');
-      link.href = '#';
-      link.className = 'active';
-      link.textContent = name; //changed from id
 
-      link.onclick = function (e) {
-          e.preventDefault();
-          e.stopPropagation();
-          for (layers in ids) {
-            var visibility = map.getLayoutProperty(ids[layers], 'visibility');
+    console.log(map.getStyle().layers)
 
-            if (visibility === 'visible') {
-                map.setLayoutProperty(ids[layers], 'visibility', 'none');
-                this.className = '';  
-            } else {
-                this.className = 'active';
-                map.setLayoutProperty(ids[layers], 'visibility', 'visible');
-                //Event listener
-                document.getElementById('slider').addEventListener('input', function(evt) {
-                    var timeIndex = parseInt(evt.target.value, 10);
-                    filterBy(link.textContent, timeIndex);
-                });
+});
 
-            }
-          }
-      };
-    
-      var layers = document.getElementById('menu');
-      layers.appendChild(link);
-      // should call filterBy
-}
-
-function filterBy(layer, timeIndex) {
- 
-  var timeRanges = [
-  ["2016-09-26 06:00:00Z", "2016-09-26 08:00:00Z"],
-  ["2016-09-26 08:00:00Z", "2016-09-26 10:00:00Z"],
-  ["2016-09-26 10:00:00Z", "2016-09-26 12:00:00Z"],
-  ["2016-09-26 12:00:00Z", "2016-09-26 14:00:00Z"]
-  ["2016-09-26 14:00:00Z", "2016-09-26 16:00:00Z"],
-  ["2016-09-26 16:00:00Z", "2016-09-26 18:00:00Z"],
-  ["2016-09-26 18:00:00Z", "2016-09-26 20:00:00Z"],
-]
-  
-  var startHour = new Date(timeRanges[timeIndex][0]).getUTCHours();
-  var endHour = new Date(timeRanges[timeIndex][1]).getUTCHours();
-  console.log(startHour, endHour); //this works
-
-  var filters = ["all",
-    ["<=", "Day_start_"+layer[0], startHour],
-    [">=", "Day_end_"+layer[0], endHour]]
-  map.setFilter(filters);
-
-}
-
-/*
-function checkRange(feature, range_start, range_end) {
-  if(range_start <= feature.properties.Day_start_M && feature.properties.Day_end_M >= range_end) {
-    return true
-  } else {
-    return false
-  }
-}
-*/
 
